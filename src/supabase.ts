9e2fbaf8b3f817a1dbe743a5978941e0fb033132
hohnmartin-hop <hohn.martin@gmail.com>
2026-09-10 10:49:49 +0200

    oprava

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('Chybí Supabase URL nebo Anon klíč v proměnných prostředí!');
-}
+const supabaseUrl = 'https://crulaktbptkfhzqywddg.supabase.co';
+const supabaseAnonKey = (import.meta.env.VITE_SUPABASE_ANON_KEY || '').trim();
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
